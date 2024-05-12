2944d56dd66346cc37ba24571a64310ada1d155e
Muzakka123 <159703338+Muzakka123@users.noreply.github.com>
2024-05-12 16:18:04 +0100

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,7 @@
+// escersice 10 ;
+// my name is zakii ;
+//Dated 03/03/2024
+// this program will multiplication ;
+
+console.log(5 * 3);
+
---
+++ main.js
@@ -0,0 +1,5 @@
+// escersice 10 ;
+// my name is zakii ;
+//Dated 03/03/2024
+// this programe will run simple code just like hellow world;
+console.log("hello world simple programe");
